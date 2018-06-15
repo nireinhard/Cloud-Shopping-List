7cc09fc9e78f6ab07e1815b30f2b7e6dfba17ca3
nireinhard <nireinhard@htwsaar.de>
2018-06-15 23:03:10 +0200

    Initial Commit

---
+++ CloudShoppingList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudShoppingList
+//
+//  Created by Niklas Reinhard on 15.06.18.
+//  Copyright © 2018 Niklas Reinhard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
